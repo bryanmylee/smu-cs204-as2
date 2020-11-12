d994bf05fa97db5f742b6529566cb75372d6de9b
Bryan Lee <bryanmylee@gmail.com>
2020-11-13 02:52:31 +0800

    Implement connect 4 functionality

---
+++ PROTOCOL.md
@@ -0,0 +1,178 @@
+# Reverse Engineering Connect 4
+
+## Deconstructing messages
+
+### Packet breakdown
+
+1 -- 16: client logging in.
+
+47 -- 50: client starting a new game.
+
+57 -- 64: client made a move (3), server responded (7).
+
+71 -- 78: client made a move (1), server responded (1).
+
+91 -- 92: client left the game.
+
+93 -- 98: client A logging in.
+
+105 -- 110: client B logging in.
+
+136 -- 139: client A starting a new game.
+
+140 -- 143: client B starting a new game.
+
+150 -- 151: client B left the game.
+
+152 -- 153: client A left the game.
+
+160 -- 165: client logging in.
+
+166 -- 169: client starting a new game.
+
+182 -- 189: client made a move (1), server made a move (5).
+
+196 -- 203: client made a move (1), server made a move (1).
+
+210 -- 217: client made a move (1), server made a move (1).
+
+218 -- 225: client made a move (7), server made a move (6).
+
+238 -- 245: client made a move (7), server made a move (7).
+
+246 -- 253: client made a move (6), server made a move (7).
+
+...
+
+300 -- 305: client made an invalid move (1)
+
+338 -- 343: client loses.
+
+380 -- 387: client starts a new game again.
+
+388 -- 389: client leaves the game.
+
+#### Logging In
+
+```
+s to c: 2734 // What type of game?
+```
+
+#### Start New Game
+
+```
+c to s: 374b // Start a new game of type 4b
+s to c: 1a34 // Confirm new game
+```
+
+#### Client Makes a Move 3, Server Responds 7
+
+```
+c to s: 3f02 // Make move at index 2
+s to c: 3c02 // Confirm move at index 2
+s to c: 5506 // Respond with index 6
+s to c: 1a34 // NOOP
+```
+
+#### Client Makes a Move 1, Server Responds 1
+
+```
+c to s: 3f00 // Make move at index 0
+s to c: 3c00 // Confirm move at index 0
+s to c: 5500 // Respond with index 0
+s to c: 1a34 // NOOP
+```
+
+#### Client Loses
+
+```
+c to c: 3f01 // Make move at index 1
+s to c: 3c01 // Confirm move at index 1
+s to c: 5502 // Respond with index 2
+s to c: 0b18 // Server wins
+```
+
+#### Client Wins
+
+```
+c to s: 3f02 // Make move at index 2
+s to c: 3c02 // Confirm move at index 2
+s to c: 0b3d // Client wins
+```
+
+#### Tie
+
+```
+c to s: 3f00 // Make move at index 0
+s to c: 3c00 // Confirm move at index 0
+s to c: 5506 // Respond with index 6
+s to c: 0b35 // It's a tie
+```
+
+#### Play Again
+
+```
+c to s: 2234 // Play again
+s to c: 2734 // What type of game?
+c to s: 374b // Start a new game of type 4b
+s to c: 1a34 // Confirm new game
+```
+
+#### Don't Play Again
+
+```
+c to s: 2a34 // Don't play again
+s to c: FIN
+```
+
+#### Forfeit
+
+```
+c to s: 4134
+s to c: 0b18jwllj
+```
+
+
+
+#### Quit
+
+```
+c to s: FIN
+```
+
+#### Invalid Move
+
+```
+c to s: 3f00 // Make move at index 0
+s to c: 2300 // Deny index 0
+s to c: 1a34 // NOOP
+```
+
+```
+c to s: 3f02 // Make move at index 2
+s to c: 2302 // Deny index 2
+s to c: 1a34 // NOOP
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,193 @@
+from datetime import datetime
+import random
+import selectors
+import socket
+import sys
+
+
+class Game:
+    # Define the game board with bitstrings.
+    # https://github.com/denkspuren/BitboardC4/blob/master/BitboardDesign.md
+    def __init__(self):
+        self.mask = 0
+        # The server's pieces for this game.
+        self.pieces = 0
+
+    @property
+    def client_pieces(self):
+        return self.mask ^ self.pieces
+
+    def win(self, is_server):
+        if is_server:
+            return self.connected_four(self.pieces)
+        return self.connected_four(self.client_pieces)
+
+    def connected_four(self, pieces):
+        # Check horizontal.
+        m = pieces & (pieces >> 7)
+        if m & (m >> 14):
+            return True
+        # Check primary diagonal.
+        m = pieces & (pieces >> 6)
+        if m & (m >> 12):
+            return True
+        # Check secondary diagonal.
+        m = pieces & (pieces >> 8)
+        if m & (m >> 16):
+            return True
+        # Check vertical.
+        m = pieces & (pieces >> 1)
+        if m & (m >> 2):
+            return True
+        return False
+
+    def is_full(self):
+        return self.mask == 279258638311359
+
+    def make_move(self, is_server, col):
+        new_piece = 1 << (col * 7)
+        # Check if column is already full.
+        if self.mask & new_piece << 5:
+            return False
+        # Add a piece to the board.
+        old_mask = self.mask
+        self.mask = self.mask | (self.mask + new_piece)
+        if is_server:
+            # Update the server's pieces.
+            self.pieces = self.pieces | (self.mask ^ old_mask)
+        return True
+
+
+class Client:
+    def __init__(self, conn):
+        self.conn = conn
+        self.game = Game()
+
+
+def get_timestamp(now=None):
+    if now is None:
+        now = datetime.now()
+    return now.strftime("%H:%M:%S")
+
+
+def get_host_port():
+    host = "127.0.0.1"
+    port = 1500
+    if len(sys.argv) >= 2:
+        port = int(sys.argv[1])
+    return host, port
+
+
+def create_async_server_socket(host, port):
+    # Create an INET, STREAM (TCP) socket.
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+    server.setblocking(False)
+    print(f"{get_timestamp()} Server waiting for Clients on port {port}.")
+    return server
+
+
+def accept_fn(server, mask, clients):
+    global sel
+    print("ACCEPTED SOMETHING")
+    conn, addr = server.accept()
+    conn.setblocking(False)
+    host, port = addr
+    client = Client(conn)
+    clients[conn.fileno()] = client
+    init_game(client)
+    sel.register(conn, selectors.EVENT_READ, listen_fn)
+
+
+def listen_fn(conn, mask, clients):
+    global sel
+    print("LISTENED TO SOMETHING")
+    # Get the matching Client object for the given connection.
+    client = clients[conn.fileno()]
+    # Non-blocking conn should be ready for just one recv.
+    data_bytes = client.conn.recv(1024)
+    if not data_bytes:
+        logout(client, clients, proper=False)
+        return
+    if data_bytes[0] == 0x37:
+        start_game(client, data_bytes[1])
+    if data_bytes[0] == 0x3f:
+        make_move(client, data_bytes[1])
+
+
+def logout(asker: Client, clients, proper=True):
+    global sel
+    try:
+        asker.conn.close()
+    except:
+        pass
+    clients.pop(asker.conn.fileno(), None)
+    sel.unregister(asker.conn)
+
+
+def init_game(client: Client):
+    global port
+    print("INIT GAME")
+    client.conn.send(bytes([0x27, 0x34]))
+
+
+def start_game(client: Client, game_type):
+    print(f"GAME TYPE {game_type}")
+    client.conn.send(bytes([0x1a, 0x34]))
+
+
+def make_move(client: Client, move):
+    print(f"CLIENT MADE MOVE {move}")
+    valid = client.game.make_move(is_server=False, col=move)
+    # Invalid move.
+    if not valid:
+        client.conn.send(bytes([0x23, move]))
+        client.conn.send(bytes([0x1a, 0x34]))
+        return
+    # Confirm move.
+    client.conn.send(bytes([0x3c, move]))
+    # Reply if client wins.
+    if client.game.win(is_server=False):
+        client.conn.send(bytes([0x0b, 0x3d]))
+        return
+    s_move = random.randint(0, 6)
+    while not client.game.make_move(is_server=True, col=s_move):
+        s_move += 1
+        s_move %= 7
+    # Reply with server move.
+    client.conn.send(bytes([0x55, s_move]))
+    # Check if server won.
+    if client.game.win(is_server=True):
+        client.conn.send(bytes([0x0b, 0x18]))
+        return
+    # Check if tie.
+    if client.game.is_full():
+        client.conn.send(bytes([0x0b, 0x35]))
+        return
+    client.conn.send(bytes([0x1a, 0x34]))
+
+
+
+if __name__ == "__main__":
+    global port
+    global sel
+    host, port = get_host_port()
+    server = create_async_server_socket(host, port)
+    # Create a selector with socket object key and a function value that
+    # accepts a socket, mutex mask, and a map from socket file number to socket.
+    sel = selectors.DefaultSelector()
+    sel.register(server, selectors.EVENT_READ, accept_fn)
+
+    # A map from a socket file number key to a Client object.
+    clients = {}
+    while True:
+        # Get all socket object keys in the selector.
+        for key, mask in sel.select():
+            # Either the accept_fn or listen_fn.
+            fn = key.data
+            conn = key.fileobj
+            fn(conn, mask, clients)
+
+
+
